f39630a2338c7407ff190d4c7b5bf08673de3d1e
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-07-03 13:11:26 -0400

    fix: batch script

--- install_docker.sh
+++ install_docker.sh
@@ -1,8 +1,8 @@
 # https://docs.docker.com/engine/install/debian/
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -14,5 +14,5 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # install docker packages
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
