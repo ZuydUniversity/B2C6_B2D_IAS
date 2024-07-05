9587d332514fc8c6e485a5e514164b66bfefc6c8
Deflation8449 <169663220+Deflation8449@users.noreply.github.com>
2024-07-05 14:30:01 -0400

    feat: added ansible create vm script

---
+++ run_ansible_create_vm.sh
@@ -0,0 +1,20 @@
+# debian 12 ansible install
+
+ansible_install() {
+    # add ansible apt repo
+    wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/ansible.list
+    sudo apt update
+    
+    # install ansible
+    sudo apt install ansible
+
+    sudo pip install ansible[azure]
+}
+
+ansible_run() {
+    ansible-playbook Playbooks/azure/vm_create.yml
+}
+
+ansible_install
+ansible_run
